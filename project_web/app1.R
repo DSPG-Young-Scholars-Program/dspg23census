4489c49d577fed85167b63c78d7f1bd6cd1a669f
JianingC0913 <129560784+JianingC0913@users.noreply.github.com>
2023-07-24 16:43:44 -0400

    update web and econ analysis file

--- project_web/app1.R
+++ project_web/app1.R
@@ -20,7 +20,7 @@ ui <-  shinyUI(navbarPage(title="State Data Use and Needs",
                         br(),
                         sidebarLayout(sidebarPanel(
                           selectInput("dropdown2", "Which state are you interested in?",
-                                        list("All states" = "all","Alaska"="AK", "Virginia"="VA", "California"= "CA")),
+                                        list("All states","Alaska", "Alabama", "California")),
                           strong("What categories are you interested in?"),
                           checkboxInput("demo", "Demographics", TRUE),
                           checkboxInput("econ", "Economy", TRUE),
@@ -68,6 +68,126 @@ server <- function(input, output) {
       "You have selected California to inspect"
     }
   })
+  
+  output$plot2 <- renderPlot({
+    if (input$dropdown2 == "All states"){
+      data <- read.csv("/Users/jianingcai/Downloads/Economy_compilation.csv")
+      employment = 0
+      lf = 0
+      wage = 0
+      income = 0
+      tax = 0
+      job = 0
+      economy = 0
+      for (i in data$Sub.categories){
+        if (any(grepl("Employment", i))){
+          employment = employment + 1
+        }
+        if (any(grepl("employment", i))){
+          employment = employment + 1
+        }
+        if (any(grepl("labor force", i))){
+          lf = lf + 1
+        }
+        if (any(grepl("Labor force", i))){
+          lf = lf + 1
+        }
+        if (any(grepl("wage", i))){
+          wage = wage + 1
+        }
+        if (any(grepl("Wage", i))){
+          wage = wage + 1
+        }
+        if (any(grepl("job", i))){
+          job = job + 1
+        }
+        if (any(grepl("Job", i))){
+          job = job + 1
+        }
+        if (any(grepl("tax", i))){
+          tax = tax + 1
+        }
+        if (any(grepl("Tax", i))){
+          tax = tax + 1
+        }
+        if (any(grepl("income", i))){
+          income = income + 1
+        }
+        if (any(grepl("Income", i))){
+          income = income + 1
+        }
+        if (any(grepl("Economy", i))){
+          economy = economy + 1
+        }
+        if (any(grepl("economy", i))){
+          economy = economy + 1
+        }
+      }
+      category <- c("employment", "income", "tax", "labor force", "wage", "job","economy")
+      counts <- c(employment, income, tax, lf, wage, job,economy)
+      total_df = data.frame(category, counts)
+      barplot(counts, names.arg = category, col = "steelblue", main = "Distribution of Data Tools' Category(All States)", xlab = "Category", ylab = "counts", cex.names = 0.9, ylim = c(0,60))
+      text(x = c(0.7, 1.9, 3.1, 4.3, 5.5, 6.7, 7.9), y = counts, labels = counts, pos = 3, cex = 0.8, col = "black")
+    }
+    else{
+      data <- read.csv("/Users/jianingcai/Downloads/Economy_compilation.csv")
+      employment = 0
+      lf = 0
+      wage = 0
+      income = 0
+      tax = 0
+      job = 0
+      economy = 0
+      for (i in data$Sub.categories[data$State..Country == input$dropdown2]){
+        if (any(grepl("Employment", i))){
+          employment = employment + 1
+        }
+        if (any(grepl("employment", i))){
+          employment = employment + 1
+        }
+        if (any(grepl("labor force", i))){
+          lf = lf + 1
+        }
+        if (any(grepl("Labor force", i))){
+          lf = lf + 1
+        }
+        if (any(grepl("wage", i))){
+          wage = wage + 1
+        }
+        if (any(grepl("Wage", i))){
+          wage = wage + 1
+        }
+        if (any(grepl("job", i))){
+          job = job + 1
+        }
+        if (any(grepl("Job", i))){
+          job = job + 1
+        }
+        if (any(grepl("tax", i))){
+          tax = tax + 1
+        }
+        if (any(grepl("Tax", i))){
+          tax = tax + 1
+        }
+        if (any(grepl("income", i))){
+          income = income + 1
+        }
+        if (any(grepl("Income", i))){
+          income = income + 1
+        }
+        if (any(grepl("Economy", i))){
+          economy = economy + 1
+        }
+        if (any(grepl("economy", i))){
+          economy = economy + 1
+        }
+      }
+      category <- c("employment", "income", "tax", "labor force", "wage", "job","economy")
+      counts <- c(employment, income, tax, lf, wage, job,economy)
+      total_df = data.frame(category, counts)
+      barplot(counts, names.arg = category, col = "steelblue", ylab = "counts", cex.names = 0.9, ylim = c(0,60))
+      text(x = c(0.7, 1.9, 3.1, 4.3, 5.5, 6.7, 7.9), y = counts, labels = counts, pos = 3, cex = 0.8, col = "black")
+  }})
   
   output$text3 <- renderText({
     if (input$dropdown3 == "all"){
